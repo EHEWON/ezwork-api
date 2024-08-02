e69607f7c3f9acf837a000f0eacbbda9f6968e2b
lemount <tvrc4m@qq.com>
2024-08-02 09:56:31 +0800

    fix

--- python/translate/pdf.py
+++ python/translate/pdf.py
@@ -381,11 +381,11 @@ def docxtopdf(docx_path, pdf_path):
     target_path_dir=os.path.dirname(pdf_path)
     if not os.path.exists(target_path_dir):
         os.makedirs(target_path_dir)
-    target_pdf = fitz.Document()
-    target_pdf.new_page()
-    target_pdf.save(pdf_path)
-    target_pdf.close()
-    subprocess.run([unoconv_path,"-f","pdf","-e","UTF-8","-o",pdf_path, docx_path])
+    # target_pdf = fitz.Document()
+    # target_pdf.new_page()
+    # target_pdf.save(pdf_path)
+    # target_pdf.close()
+    subprocess.run([unoconv_path,"-f","pdf","-e","UTF-8","-o",target_path_dir, docx_path])
 
    
 
