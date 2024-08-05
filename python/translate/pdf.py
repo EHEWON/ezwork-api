0b201c49b65dec12cef8deebe637206ad9e25b23
lemount <tvrc4m@qq.com>
2024-08-05 16:13:57 +0800

    test

--- python/translate/pdf.py
+++ python/translate/pdf.py
@@ -388,7 +388,7 @@ def docxtopdf(docx_path, pdf_path):
     # subprocess.run([unoconv_path,"-f","pdf","-e","UTF-8","-o",target_path_dir, docx_path])
     # subprocess.run([unoconv_path,"-f","pdf","-e","UTF-8","-o",target_path_dir, docx_path])
     pdf_path="/tmp/222.pdf"
-    print("sudo {} -f pdf -o {} {}".format(unoconv_path,pdf_path, docx_path))
+    print("sudo {} -f1 pdf -o {} {}".format(unoconv_path,pdf_path, docx_path))
     try:
         subprocess.run(["sudo",unoconv_path,"-f","pdf","-i","utf8","-o","222.pdf", docx_path], shell=True)
     except Exception as e:
