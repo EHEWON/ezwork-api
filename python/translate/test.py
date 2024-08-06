c3a416fb54f962db3baa53e052c12ce4923ad7f5
lemount <tvrc4m@qq.com>
2024-08-06 15:32:04 +0800

    test

--- python/translate/test.py
+++ python/translate/test.py
@@ -1,26 +1,39 @@
 import subprocess
 import shutil
+import getpass
 
 unoconv_path = shutil.which("unoconv")
 docx_path="/Volumes/data/erui/ezwork-api/storage/app/public/translate/ehBTbaeCbdMPJdV7B9lV1zewG3KM5H7agVk6KIIQ/avm-1-俄语.docx"
 pdf_path="/Volumes/data/erui/ezwork-api/storage/app/public/translate/ehBTbaeCbdMPJdV7B9lV1zewG3KM5H7agVk6KIIQ/avm-1-俄语.pdf"
 
-print("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path))
+username = getpass.getuser()
+print(f"当前执行用户是: {username}\n")
+
+print("\n{} -f pdf -o {} {}\n".format(unoconv_path, pdf_path, docx_path))
 try:
     process=subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path), stdout=subprocess.PIPE, text=True)
     print(process.stdout)
-    print("done")
+    print("done\n")
 except Exception as e:
     print(e)
 
 try:
+    print("\nstart2")
     subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path))
-    print("done2")
+    print("\ndone2")
 except Exception as e:
     print(e)
 
 try:
+    print("\nstart3")
     subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path),shell=True)
-    print("done3")
+    print("\ndone3")
+except Exception as e:
+    print(e)
+
+try:
+    print("\nstart4")
+    subprocess.run("{} -f pdf {}".format(unoconv_path, docx_path),shell=True)
+    print("\ndone4")
 except Exception as e:
     print(e)
