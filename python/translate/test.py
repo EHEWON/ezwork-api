fd8238a4bce06d23363503b98e6fcc79c89316a5
lemount <tvrc4m@qq.com>
2024-08-06 11:18:28 +0800

    test

--- python/translate/test.py
+++ python/translate/test.py
@@ -6,8 +6,5 @@ docx_path="/Volumes/data/erui/ezwork-api/storage/app/public/translate/9WHoPMLc5A
 pdf_path="/Volumes/data/erui/ezwork-api/storage/app/public/translate/9WHoPMLc5Acl503WVzfF3eV3V4evq0xN35SnAJv6/avm-中文.pdf"
 
 print(unoconv_path)
-
-try:
-    subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path), shell=True)
-except Exception as e:
-    print(e)
+print("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path))
+subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path), shell=True)
