3230b11bca9523aeb8019088844c296460390d0a
lemount <tvrc4m@qq.com>
2024-08-13 13:36:07 +0800

    test

--- python/translate/test.py
+++ python/translate/test.py
@@ -2,17 +2,17 @@ import subprocess
 import shutil
 import getpass
 
-# unoconv_path = shutil.which("unoconv")
-unoconv_path = "sudo /usr/local/bin/unoconv -vvv "
-docx_path="/data/www/ezwork_api/storage/app/public/translate/ehBTbaeCbdMPJdV7B9lV1zewG3KM5H7agVk6KIIQ/avm-1-俄语.docx"
-pdf_path="/data/www/ezwork_api/storage/app/public/translate/ehBTbaeCbdMPJdV7B9lV1zewG3KM5H7agVk6KIIQ/avm-1-俄语.pdf"
+unoconv_path = shutil.which("unoconv")
+# unoconv_path = "sudo /usr/local/bin/unoconv -vvv "
+docx_path="/data/www/ezwork_api/storage/app/public/translate/JRatLaQlYcWm4zAKC5NefpXh3cmG3y7cJ6EC58CU/面试评估表模板-日语.docx"
+pdf_path="/data/www/ezwork_api/storage/app/public/translate/JRatLaQlYcWm4zAKC5NefpXh3cmG3y7cJ6EC58CU/面试评估表模板-日语.pdf"
 
 username = getpass.getuser()
 print(f"当前执行用户是: {username}\n")
 
 print("\n{} -f pdf -o {} {}\n".format(unoconv_path, pdf_path, docx_path))
 try:
-    process=subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path), stdout=subprocess.PIPE, text=True)
+    process=subprocess.run("{} -vvv -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path), stdout=subprocess.PIPE, text=True)
     print(process.stdout)
     print("done\n")
 except Exception as e:
@@ -20,21 +20,21 @@ except Exception as e:
 
 try:
     print("\nstart2")
-    subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path))
+    subprocess.run("{} -vvv -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path))
     print("\ndone2")
 except Exception as e:
     print(e)
 
 try:
     print("\nstart3")
-    subprocess.run("{} -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path),shell=True)
+    subprocess.run("{} -vvv -f pdf -o {} {}".format(unoconv_path, pdf_path, docx_path),shell=True)
     print("\ndone3")
 except Exception as e:
     print(e)
 
 try:
     print("\nstart4")
-    subprocess.run("{} -f pdf {}".format(unoconv_path, docx_path),shell=True)
+    subprocess.run("{} -vvv -f pdf {}".format(unoconv_path, docx_path),shell=True)
     print("\ndone4")
 except Exception as e:
     print(e)
